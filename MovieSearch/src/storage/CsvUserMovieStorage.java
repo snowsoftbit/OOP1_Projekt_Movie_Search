cd6cb046bf0908e66b2f5ed588b21320b7a3725f
carmarkie <88bf48hn5w@privaterelay.appleid.com>
2026-01-06 11:06:25 +0100

    Update CsvUserMovieStorage.java

--- MovieSearch/src/storage/CsvUserMovieStorage.java
+++ MovieSearch/src/storage/CsvUserMovieStorage.java
@@ -1,5 +1,36 @@
 package storage;
 
+import model.Movie;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 public class CsvUserMovieStorage {
 
+    private final Path filePath;
+
+    public CsvUserMovieStorage(String path) {
+        this.filePath = Paths.get(path);
+    }
+
+    public void saveMovie(Movie movie) throws IOException {
+        // Ordner anlegen, falls er fehlt
+        if (filePath.getParent() != null) {
+            Files.createDirectories(filePath.getParent());
+        }
+
+        // Film ans Ende der Datei schreiben
+        try (BufferedWriter writer = Files.newBufferedWriter(
+                filePath,
+                StandardOpenOption.CREATE,
+                StandardOpenOption.APPEND
+        )) {
+            writer.write(movie.getImdbId() + "," + movie.getTitle() + "," + movie.getDirector());
+            writer.newLine();
+        }
+    }
 }
